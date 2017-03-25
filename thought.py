2282f64e16cf7cac33a1353ba273a2c9ce3de323
Tristan Ray <tristanray@0587388206.wireless.umich.net>
2017-03-25 17:02:48 -0400

    Thought action counter and contstant time for actions.

--- thought.py
+++ thought.py
@@ -1,2 +1,33 @@
-def process_thoughts():
-    pass
+import constants
+import action
+
+#player.current_action
+#player.action_counter
+#player.last_thought
+
+def process_thoughts(player):
+    
+    if player.action_counter < player.current_action_timer
+    	return
+
+    
+
+    #skip if current thought count hasn't reached max
+
+    #think about current thought
+
+    #check field of view: make variable
+
+    #think about exploring
+
+
+
+
+
+
+
+
+    #think about eating
+    #think about drinking
+
+
