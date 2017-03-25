2ca2c26d01b5519fce6aabc640446c2d69e8fbf4
Tristan Ray <tlray@umich.edu>
2017-03-25 17:45:14 -0400

    comment out all of process_thoughts()

--- thought.py
+++ thought.py
@@ -4,27 +4,28 @@ import action
 #player.inventory
 
 def process_thoughts(player):
+	pass
     
-    if player.action_counter < constants.ACTION_TIME[player.current_action]:
-    	return
+ #    if player.action_counter < constants.ACTION_TIME[player.current_action]:
+ #    	return
 
-	if player.hunger > constants.HUNGER_THRESHOLD:
-		for invIndex in player.inventory:
-			#if player.inventory[invIndex] is edible:
-				#player.current_action = "eat"
-				return
-		if 
-
-
+	# if player.hunger > constants.HUNGER_THRESHOLD:
+	# 	for invIndex in player.inventory:
+	# 		#if player.inventory[invIndex] is edible:
+	# 			#player.current_action = "eat"
+	# 			return
+	# 	if 
 
 
-	elif player.thirst > constants.THIRST_THRESHOLD
-		for invIndex in player.inventory:
-			#if player.inventory[invIndex] is edible:
-				#player.current_action = "eat"
 
-	elif player.energy < ENERGY_THRESHOLD    
-		player.current_action = "sleep"
+
+	# elif player.thirst > constants.THIRST_THRESHOLD:
+	# 	for invIndex in player.inventory:
+	# 		#if player.inventory[invIndex] is edible:
+	# 			#player.current_action = "eat"
+
+	# elif player.energy < ENERGY_THRESHOLD: 
+	# 	player.current_action = "sleep"
 
     #skip if current thought count hasn't reached max
 
