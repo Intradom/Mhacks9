808457d6f90b2ca6056570b554afbf3dd1a7c933
Tristan Ray <tristanray@0587388206.wireless.umich.net>
2017-03-25 13:43:04 -0400

    Player Constants

--- constants.py
+++ constants.py
@@ -8,3 +8,19 @@ C_BLACK = (0, 0, 0)
 SCREEN_WIDTH = 640      # Pixels
 SCREEN_HEIGHT = 480     # Pixels
 CYCLE_DELAY = 1000      # Delay in milliseconds
+
+# Player Initial Stats
+HUNGER = 1
+THIRST = 1
+ENERGY = 100
+FITNESS = 50 
+ANGER = 50
+FEAR = 50
+GRIEF = 50
+JOY = 50
+INTELLIGENCE = 50
+SOBRIETY = 100
+BLADDER = 1
+DUMP = 1
+
+# Thing Initial
