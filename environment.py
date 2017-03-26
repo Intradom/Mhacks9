2b92bca2394ae8826cde2b454ee31b8285fa4daf
Mel Savich <msavich@umich.edu>
2017-03-25 23:41:58 -0400

    Implemented update tree:

--- environment.py
+++ environment.py
@@ -1,2 +1,10 @@
-def process_environment():
-    pass
+import classes
+
+
+def process_environment(environment):
+    for x in range(len(environment.global_map)):
+        for y in range(len(environment.global_map[0])):
+            name = environment.global_map[x][y]
+            num_resources = environment.global_map[x][y].resources
+            if name == "apple_tree" and num_resources == 0:
+                classes.environment.change_tile("reg_tree", 0, x, y)
